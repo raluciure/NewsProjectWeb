6a0907bcb4a84b414f3c15ed4814ceb0e9c64bdf
raluciure <ralu.ciure@gmail.com>
2022-10-17 02:06:57 +0200

    Added dropdown for category selection

--- src/app/article-edition/article-edition.component.ts
+++ src/app/article-edition/article-edition.component.ts
@@ -4,6 +4,8 @@ import { Article } from '../interfaces/article';
 import { NewsService } from '../services/news.service';
 import { Location } from '@angular/common';
 import { NgForm } from '@angular/forms';
+import { LoginService } from '../services/login.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-edit-article',
@@ -16,12 +18,12 @@ export class ArticleEditionComponent implements OnInit {
 
   @ViewChild('articlesForm') articleForm: any;
 
-  constructor(private article_service: NewsService, private route: ActivatedRoute, private location: Location, private router: ActivatedRoute) {
+  constructor(private article_service: NewsService, private loginService: LoginService, private route: ActivatedRoute, private location: Location, private router: Router) {
     this.article = { id: 0, title: "", subtitle: "", abstract: "", body: "", category: "" };
   }
 
   ngOnInit(): void {
-    const article_id = this.router.snapshot.paramMap.get("article_id");
+    const article_id = this.route.snapshot.paramMap.get("article_id");
     this.article_service.getArticle(Number(article_id))
       .subscribe(article => {
         this.article = article;
@@ -29,19 +31,41 @@ export class ArticleEditionComponent implements OnInit {
   }
 
   edit(articleForm: NgForm, article: Article) {
-    article.title = articleForm.value.ntitle;
-    article.subtitle = articleForm.value.nsubtitle;
-    article.abstract = articleForm.value.nabstract;
-    article.body = articleForm.value.nbody;
-    console.log('Edited One is', article);
-    this.article_service.updateArticle(article).subscribe(
-      _ => {
-        this.message = 'Article edited successfully';
-      },
-      err => {
-        this.message = `An error has ocurred: ${err.statusText}`;
+    Swal.fire({
+      title: 'Are you sure you want to save the changes?',
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: `Yes`,
+      denyButtonText: `No`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        article.title = articleForm.value.ntitle;
+        article.subtitle = articleForm.value.nsubtitle;
+        article.abstract = articleForm.value.nabstract;
+        article.body = articleForm.value.nbody;
+        console.log('Edited One is', article);
+        this.article_service.updateArticle(article).subscribe(
+          _ => {
+            this.message = 'Article edited successfully!';
+            this.redirectTo(`/articles`)
+          },
+          err => {
+            this.message = `An error has ocurred: ${err.statusText}`;
+          });
+          Swal.fire('Article edited successfully!', '', 'success');
       }
-    );
+      else if (result.isDenied) {
+        Swal.fire('Changes are not saved', '', 'info')
+      }
+    });
+  }
+
+  setCategory(event: any, article: Article) {
+    article.category = event.target.value;
+  }
+
+  isLoggedIn(): boolean {
+    return this.loginService.isLogged();
   }
 
   reset(): void {
@@ -50,6 +74,11 @@ export class ArticleEditionComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  redirectTo(uri: string) {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() =>
+      this.router.navigate([uri]));
   }
 
 }
